a64567812e9739ad6702c83b05d663ffa0b24450
utkrishtsingh739-star <utkrishtsingh739@gmail.com>
2026-02-17 23:12:23 +0530

    Filtering and Sorting

---
+++ Filtering and Sorting.sql
@@ -0,0 +1,70 @@
+Create database Company;
+Use company;
+Create table Employees
+(EmpID Int Primary key,
+EmpName Varchar(50),
+Department Varchar (30),
+City Varchar (20),
+Salary Int,
+HireDate Date);
+
+Insert into Employees
+Values (101,"Rahul Mehta","Sales","Delhi",55000,"2020-04-12"),
+        (102,"Priya Sharma","HR","Mumbai",62000,"2019-09-25"),
+        (103,"Aman Singh","IT","Bengaluru",72000,"2021-03-10"),
+        (104,"Neha Patel","Sales","Delhi",48000,"2022-01-14"),
+        (105,"Karan Joshi","Marketing","Pune","45000","2018-07-22"),
+        (106,"Divya Nair","IT","Chennai",81000,"2019-12-11"),
+        (107,"Raj Kumar","HR","Delhi",60000,"2020-05-28"),
+        (108,"Simran Kaur","Finance","Mumbai",58000,"2021-08-03"),
+        (109,"Arjun Reddy","IT","Hyderabad",70000,"2022-02-18"),
+        (110,"Anjali Das","Sales","Kolkata",51000,"2023-01-15");
+        
+        Select * from Employees;
+        
+        ##Question 1 : Show employees working in either the ‘IT’ or ‘HR’ departments.
+        
+        Select * from Employees
+        where Department IN ('IT','HR');
+        
+        ##Question 2 : Retrieve employees whose department is in ‘Sales’, ‘IT’, or ‘Finance’.
+        
+	SELECT * FROM Employees
+    WHERE Department IN ('Sales', 'IT', 'Finance');
+    
+    ##Question 3 : Display employees whose salary is between ₹50,000 and ₹70,000.
+    
+    Select * from Employees
+    where Salary between 50000 and 70000;
+    
+    ##Question 4 : List employees whose names start with the letter ‘A’.
+    
+    Select * from Employees
+    where empName like 'A%';
+    
+    ##Question 5 : Find employees whose names contain the substring ‘an’.
+    
+    Select * from Employees
+    where empName like '%an%';
+    
+    ##Question 6 : Show employees who are from ‘Delhi’ or ‘Mumbai’ and earn more than ₹55,000.
+    
+    Select * from Employees
+    where city in('Delhi','Mumbai')
+    and Salary>55000;
+    
+    ##Question 7 : Display all employees except those from the ‘HR’ department.
+    
+    Select * from Employees
+    where Department <> 'HR';
+    
+    ##Question 8 : Get all employees hired between 2019 and 2022, ordered by HireDate (oldest first).
+    
+    Select * from Employees
+    where HireDate BETWEEN '2019-01-01' AND '2022-12-31'
+ORDER BY HireDate;
+    
+
+        
+        
+                
